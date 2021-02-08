9a3787ef4e8453fea4b249d19b2cf331cee4a53c
imattau <imatt.au@protonmail.com>
2021-02-08 20:21:02 +1100

    Add files via upload

---
+++ rss2gemini.py
@@ -0,0 +1,125 @@
+from time import sleep
+import asyncio
+
+from html2text import HTML2Text
+import feedparser
+from bs4 import BeautifulSoup
+from configparser import ConfigParser
+
+feedCfg = ConfigParser()
+feedCfg.read('config.ini')
+
+serverCfg = ConfigParser()
+serverCfg.read('server.ini')
+
+contentdir = serverCfg['SERVERINFO']['basedir']
+feeddir = serverCfg['SERVERINFO']['feeddir']
+
+
+'''
+def cleanDate(mDate):
+    mDate.replace(",","-")
+    mDate.replace(" ", "-")
+    mDate.replace(":", "-")
+    return mDate
+'''
+
+def buildEntry(title, htmlarticle, link):
+    title = "## " + title
+    link = "=> " + link
+    h = HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+    article = h.handle(htmlarticle)
+    
+    entry = "\n" + title + "\n" + article + "\n" + link + "\n"
+    
+    return entry
+
+def createGMIFile(gmiEntry, rssname, updated):
+    filename = contentdir + feeddir + rssname + ".gmi"
+    with open(filename, 'a') as writer:
+        writer.write(gmiEntry)
+        print('File Updated')
+      
+    
+
+def updateIndex():
+    indexFile = contentdir + feeddir + 'index.gmi'
+
+    with open(indexFile, 'w') as header:
+        header.write('## RSS Feeds \n')
+
+    for item in feedCfg.sections():
+        link = "=> " + feeddir + item + '.gmi ' + feedCfg[item]['feedtitle'] + " Updated: " + feedCfg[item]['modified'] + "\n"
+        with open(indexFile, 'a') as update:
+            update.write(link)
+
+async def rss2text():
+    
+
+    for fn in feedCfg.sections():
+        print(fn)
+        relLink = ""
+        if feedCfg[fn]['modified'] == "":
+            feed = feedparser.parse(feedCfg[fn]['url'])
+            feedCfg[fn]['modified'] = feed.modified
+            print(feedCfg[fn]['modified'])
+            with open('config.ini', 'w') as cfgWriter:
+                feedCfg.write(cfgWriter)
+            
+            
+
+            for entry in feed.entries:
+                
+                title = entry.title
+                htmlarticle = ""
+                link = entry.link
+                if 'content' in entry:
+                    htmlarticle = BeautifulSoup(entry.content[0].value, "html5lib").prettify()
+                    #print(htmlarticle) 
+                elif 'description' in entry:
+                    htmlarticle = BeautifulSoup(entry.description, "html5lib").prettify()
+                    #print('Has description')
+                #Build up the GMI file entry with the correct formatting
+                gmiEntry = buildEntry(title, htmlarticle, link)
+                
+                createGMIFile(gmiEntry, fn, feed.modified)
+                
+        else:
+            feed = feedparser.parse(feedCfg[fn]['url'], modified=feedCfg[fn]['modified'])
+               
+            if feed == {}:
+                print('No new feed items')
+                #Do nothing as the feed has not been updated
+                continue
+            elif hasattr(feed, 'modified') and feed.modified < feedCfg[fn]['modified']:
+                for entry in feed.entries:
+                
+                    title = entry.title
+                    htmlarticle = ""
+                    link = entry.link
+                    if 'content' in entry:
+                        htmlarticle = BeautifulSoup(entry.content[0].value, "html5lib").prettify()
+                        #print(htmlarticle) 
+                    elif 'description' in entry:
+                        htmlarticle = BeautifulSoup(entry.description, "html5lib").prettify()
+                        #print('Has description')
+                    #Build up the GMI file entry with the correct formatting
+                    gmiEntry = buildEntry(title, htmlarticle, link)
+                    
+                    createGMIFile(gmiEntry, fn, feed.modified)
+        updateIndex()                    
+        sleeptime = int(serverCfg['SERVERINFO']['sleeptime']) / len(feedCfg.items())
+        print('Sleeping for ', sleeptime)
+        await asyncio.sleep(sleeptime)
+        asyncio.ensure_future(rss2text())
+        
+async def main():
+    await rss2text()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(main())
+    loop.run_forever()
+    
